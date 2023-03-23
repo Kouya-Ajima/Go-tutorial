196b1e61a8797ead9ba655b1a0247fdfb9684513
koya <azumabass@gmail.com>
2023-03-24 08:15:39 +0900

    ポインターの学習を開始

--- section3_structs, slices, and maps/chapter1/main.go
+++ section3_structs, slices, and maps/chapter1/main.go
@@ -5,6 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println()
+
+	i, j := 42, 2701
+
+	// ｉへのポインター
+	p := &i
+	// ｐの参照先
+	fmt.Println(*p)
+
+	*p = 21
+	fmt.Println(*p)
+
+	p = &j
+	fmt.Println(*p)
+
+	*p = *p / 37
+	fmt.Println(*p)
+	fmt.Println(j)
 
 }
